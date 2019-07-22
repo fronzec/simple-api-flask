46b915d4242a532df86fb05fbe36a28b3d6c77a0
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2019-07-22 00:02:43 -0600

    additional code style

--- api.py
+++ api.py
@@ -4,11 +4,13 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class User(Resource):
     def get(self):
-        return {'name' : 'Eduardo'}
+        return {'name': 'Eduardo'}
+
 
 api.add_resource(User, '/user')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
