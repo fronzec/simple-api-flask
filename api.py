832e7e6b2a99aba8ffcea4f45940c5a596fe56af
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2019-07-23 00:50:34 -0600

    new: hello world rest api

--- api.py
+++ api.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
+
+api.add_resource(HelloWorld, "/hello")
 
 if __name__ == '__main__':
+    # Never use debug debug mode on production environment
     app.run(debug=True)
